716da57a318c82086a4b26a38cb41436a0795590
Landon Bullard <landonbullard29@gmail.com>
2018-06-12 05:55:48 -0400

    Awesome added to HelloWorld

--- Project1/src/dev1/HelloWorld.java
+++ Project1/src/dev1/HelloWorld.java
@@ -3,7 +3,7 @@ package dev1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello you people");
+		System.out.println("hello you awesome people");
 
 	}
 
